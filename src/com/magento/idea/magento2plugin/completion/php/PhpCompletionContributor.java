0308a075da9e5bbc9637ba86641f0c94d669c541
Vitaliy Boyko <v.boyko@atwix.com>
2020-11-01 20:01:59 +0200

    Adjusted PhpCompletionContributor for module names in the config.php

--- src/com/magento/idea/magento2plugin/completion/php/PhpCompletionContributor.java
+++ src/com/magento/idea/magento2plugin/completion/php/PhpCompletionContributor.java
@@ -2,17 +2,31 @@ package com.magento.idea.magento2plugin.completion.php;
 
 import com.intellij.codeInsight.completion.CompletionContributor;
 import com.intellij.codeInsight.completion.CompletionType;
+import com.intellij.patterns.PlatformPatterns;
 import com.jetbrains.php.lang.lexer.PhpTokenTypes;
+import com.jetbrains.php.lang.psi.elements.PhpPsiElement;
+import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
+import com.jetbrains.php.lang.psi.elements.impl.ArrayHashElementImpl;
 import com.magento.idea.magento2plugin.completion.provider.ModuleNameCompletionProvider;
 import static com.intellij.patterns.PlatformPatterns.psiElement;
+import static com.intellij.patterns.StandardPatterns.string;
 
 public class PhpCompletionContributor extends CompletionContributor {
     public PhpCompletionContributor() {
-        /* TODO: Works for any single quoted string in any PHP file */
+
         /* Module name in app/etc/config.php */
         extend(
                 CompletionType.BASIC,
-                psiElement(PhpTokenTypes.STRING_LITERAL_SINGLE_QUOTE),
+                psiElement(PhpTokenTypes.STRING_LITERAL_SINGLE_QUOTE)
+                    .withSuperParent(6,
+                        PlatformPatterns.psiElement(ArrayHashElementImpl.class)
+                            .withChild(PlatformPatterns.psiElement(PhpPsiElement.class)
+                                .withChild(PlatformPatterns.psiElement(
+                                    StringLiteralExpression.class
+                                ).withText(string().contains("modules"))
+                            )
+                        )
+                    ),
                 new ModuleNameCompletionProvider()
         );
     }
